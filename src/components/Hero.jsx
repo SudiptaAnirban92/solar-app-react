9053078e3765379828796680f039d6db7e49710f
Sudipta Anirban <sudiptaanirban@gmail.com>
2024-06-12 10:51:16 +0600

    logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { hero } from "../assets";
-
 const Hero = () => {
   return (
-    <section id="home" className="w-full max-h-[800px] py-0 2xl:py-6 mx-auto relative">
+    <section
+      id="home"
+      className="w-full max-h-[800px] py-0 2xl:py-6 mx-auto relative"
+    >
       {/* overlay */}
       <div className="absolute w-full h-full text-primary max-h-[800px] bg-black/60 flex flex-col justify-center">
         <h1 className="px-6 text-4xl sm:text-6xl md:text-8xl font-semibold">
           Solar <br />
-          <span className="text-tertiary">Solutions</span> for <br /> a Sustainable <br />
+          <span className="text-tertiary">Solutions</span> for <br /> a
+          Sustainable <br />
           Tomorrow <span className="text-tertiary">.</span>
         </h1>
       </div>
