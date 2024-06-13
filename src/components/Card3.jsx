b872b41c1e641f8faa1aa7fb24f48df948f94de3
Sudipta Anirban <sudiptaanirban@gmail.com>
2024-06-13 23:10:16 +0600

    contacts

---
+++ src/components/Card3.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { service1 } from "../assets";
+
+const Card3 = () => {
+  return (
+    <div className="flex flex-wrap flex-col lg:flex-row">
+      {/* <div className="flex flex-col flex-wrap">
+        <div>
+
+        <img src={service1} alt="" />
+        <h3>Solar Solutions</h3>
+        <p>Harness the power of the sun and embrace clean, renewable energy with our solar solutions.</p>
+        </div>
+      </div>
+      <div></div>
+      <div></div> */}
+    </div>
+  );
+};
+
+export default Card3;
